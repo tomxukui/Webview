efbaea0067203dfcfab8740e26343ce59633e20a
xukui <xukui@wondersgroup.com>
2019-07-24 17:16:28 +0800

    init

--- app/src/main/java/com/xukui/webview/MainActivity.java
+++ app/src/main/java/com/xukui/webview/MainActivity.java
@@ -4,14 +4,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.xukui.webview.crosswalk.CrosswalkActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btn_crosswalk;
+    private RadioGroup rg_website;
+    private EditText et_custom;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,18 +29,57 @@ public class MainActivity extends AppCompatActivity {
 
     private void initView() {
         btn_crosswalk = findViewById(R.id.btn_crosswalk);
+        rg_website = findViewById(R.id.rg_website);
+        et_custom = findViewById(R.id.et_custom);
     }
 
     private void setView() {
+        rg_website.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                et_custom.setVisibility(checkedId == R.id.rb_custom ? View.VISIBLE : View.GONE);
+            }
+
+        });
+
         btn_crosswalk.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, CrosswalkActivity.class);
+                String address = getAddress();
+
+                if (TextUtils.isEmpty(address)) {
+                    Toast.makeText(MainActivity.this, "地址不能为空", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                Intent intent = CrosswalkActivity.buildIntent(MainActivity.this, address);
                 startActivity(intent);
             }
 
         });
+    }
+
+    private String getAddress() {
+        switch (rg_website.getCheckedRadioButtonId()) {
+
+            case R.id.rb_baidu: {
+                return "https://www.baidu.com";
+            }
+
+            case R.id.rb_youku: {
+                return "https://www.youku.com";
+            }
+
+            case R.id.rb_custom: {
+                return et_custom.getText().toString().trim();
+            }
+
+            default:
+                return null;
+
+        }
     }
 
 }
--- app/src/main/java/com/xukui/webview/crosswalk/CrosswalkActivity.java
+++ app/src/main/java/com/xukui/webview/crosswalk/CrosswalkActivity.java
@@ -1,5 +1,7 @@
 package com.xukui.webview.crosswalk;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -9,14 +11,23 @@ import org.xwalk.core.XWalkView;
 
 public class CrosswalkActivity extends AppCompatActivity {
 
+    private static final String EXTRA_ADDRESS = "EXTRA_ADDRESS";
+
     private XWalkView webView;
+
+    private String mAddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crosswalk);
+        initData();
         initView();
         setView();
+    }
+
+    private void initData() {
+        mAddress = getIntent().getStringExtra(EXTRA_ADDRESS);
     }
 
     private void initView() {
@@ -24,7 +35,13 @@ public class CrosswalkActivity extends AppCompatActivity {
     }
 
     private void setView() {
-        webView.loadUrl("https://www.baidu.com");
+        webView.loadUrl(mAddress);
+    }
+
+    public static Intent buildIntent(Context context, String address) {
+        Intent intent = new Intent(context, CrosswalkActivity.class);
+        intent.putExtra(EXTRA_ADDRESS, address);
+        return intent;
     }
 
 }
