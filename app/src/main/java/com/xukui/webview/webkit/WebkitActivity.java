c166b5c222767b2aead7c1fc6973a62c45acd1cc
xukui <xukui@wondersgroup.com>
2019-07-24 17:44:23 +0800

    init

--- app/src/main/java/com/xukui/webview/webkit/WebkitActivity.java
+++ app/src/main/java/com/xukui/webview/webkit/WebkitActivity.java
@@ -4,6 +4,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 import com.xukui.webview.R;
@@ -23,6 +27,8 @@ public class WebkitActivity extends AppCompatActivity {
         initData();
         initView();
         setView();
+
+        webView.loadUrl(mAddress);
     }
 
     private void initData() {
@@ -34,7 +40,42 @@ public class WebkitActivity extends AppCompatActivity {
     }
 
     private void setView() {
-        webView.loadUrl(mAddress);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);//设置js可以直接打开窗口，如window.open()，默认为false
+        webSettings.setJavaScriptEnabled(true);//是否允许JavaScript脚本运行，默认为false。设置true时，会提醒可能造成XSS漏洞
+        webSettings.setSupportZoom(true);//是否可以缩放，默认true
+        webSettings.setBuiltInZoomControls(true);//是否显示缩放按钮，默认false
+        webSettings.setUseWideViewPort(true);//设置此属性，可任意比例缩放。大视图模式
+        webSettings.setLoadWithOverviewMode(true);//和setUseWideViewPort(true)一起解决网页自适应问题
+        webSettings.setAppCacheEnabled(true);//是否使用缓存
+        webSettings.setDomStorageEnabled(true);//开启本地DOM存储
+        webSettings.setLoadsImagesAutomatically(true); // 加载图片
+        webSettings.setMediaPlaybackRequiresUserGesture(false);//播放音频，多媒体需要用户手动？设置为false为可自动播放
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (webView != null) {
+            webView.loadDataWithBaseURL(null, "", "text/html", "utf-8", null);
+            webView.clearHistory();
+            ViewParent viewParent = webView.getParent();
+            if (viewParent != null && viewParent instanceof ViewGroup) {
+                ((ViewGroup) viewParent).removeView(webView);
+            }
+            webView.destroy();
+            webView = null;
+        }
+        super.onDestroy();
     }
 
     public static Intent buildIntent(Context context, String address) {
