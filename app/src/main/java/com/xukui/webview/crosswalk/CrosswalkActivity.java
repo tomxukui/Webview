3b18d27202665147f8b077576ea8dcb6c08fc16d
xukui <xukui@wondersgroup.com>
2019-07-25 09:14:13 +0800

    init

--- app/src/main/java/com/xukui/webview/crosswalk/CrosswalkActivity.java
+++ app/src/main/java/com/xukui/webview/crosswalk/CrosswalkActivity.java
@@ -7,6 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.xukui.webview.R;
 
+import org.xwalk.core.XWalkPreferences;
+import org.xwalk.core.XWalkSettings;
 import org.xwalk.core.XWalkView;
 
 public class CrosswalkActivity extends AppCompatActivity {
@@ -24,6 +26,8 @@ public class CrosswalkActivity extends AppCompatActivity {
         initData();
         initView();
         setView();
+
+        webView.loadUrl(mAddress);
     }
 
     private void initData() {
@@ -35,7 +39,28 @@ public class CrosswalkActivity extends AppCompatActivity {
     }
 
     private void setView() {
-        webView.loadUrl(mAddress);
+        //支持javascript
+        XWalkPreferences.setValue("enable-javascript", true);
+        //开启调式,支持谷歌浏览器调式
+        XWalkPreferences.setValue(XWalkPreferences.REMOTE_DEBUGGING, true);
+        //设置是否允许通过file url加载的Javascript可以访问其他的源,包括其他的文件和http,https等其他的源
+        XWalkPreferences.setValue(XWalkPreferences.ALLOW_UNIVERSAL_ACCESS_FROM_FILE, true);
+        //支持javascript打开窗口
+        XWalkPreferences.setValue(XWalkPreferences.JAVASCRIPT_CAN_OPEN_WINDOW, true);
+        //开启多窗口
+        XWalkPreferences.setValue(XWalkPreferences.SUPPORT_MULTIPLE_WINDOWS, true);
+
+        //设置滑动
+        webView.setHorizontalScrollBarEnabled(false);
+        webView.setVerticalScrollBarEnabled(false);
+        webView.setScrollBarStyle(XWalkView.SCROLLBARS_OUTSIDE_INSET);
+        webView.setScrollbarFadingEnabled(true);
+
+        //设置缩放
+        XWalkSettings settings = webView.getSettings();
+        settings.setSupportSpatialNavigation(true);
+        settings.setBuiltInZoomControls(true);
+        settings.setSupportZoom(true);
     }
 
     @Override
